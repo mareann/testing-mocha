888057af5c7470ea9f0c7689422a0eb85e85a1f5
mareann <majgfi@gmail.com>
2020-01-19 21:39:05 -0600

    update test

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 describe("pow", function() {
 
-  it("2 raises to 3rd power", function() {
-    assert.equal(pow(2, 3), 8);
-  });
+  function makeTest(x) {
+    let expected = x * x * x;
+    it(`${x} in the power 3 is ${expected}`, function() {
+      assert.equal(pow(x, 3), expected);
+    });
+  }
 
-  it("2 raises to 2nd power", function() {
-    assert.equal(pow(2, 2), 4);
-  });
+  for (let x = 1; x <= 5; x++) {
+    makeTest(x);
+  }
+
 });
