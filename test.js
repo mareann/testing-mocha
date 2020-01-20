177aa6d391b41fdaccdc3f80ab092f094a85e764
mareann <majgfi@gmail.com>
2020-01-19 22:02:00 -0600

    update

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
+/*
+There are other assertions in Chai as well, for instance:
+
+    assert.equal(value1, value2) – checks the equality value1 == value2.
+    assert.strictEqual(value1, value2) – checks the strict equality value1 === value2.
+    assert.notEqual, assert.notStrictEqual – inverse checks to the ones above.
+    assert.isTrue(value) – checks that value === true
+    assert.isFalse(value) – checks that value === false
+    …the full list is at https://www.chaijs.com/api/assert/
+
+*/
 describe("pow", function() {
 
   function makeTest(x) {
@@ -11,4 +22,17 @@ describe("pow", function() {
     makeTest(x);
   }
 
+  it("for negative n the result is NaN", function() {
+    assert.isNaN(pow(2, -1));
+  });
+
+  it("for non-integer n the result is NaN", function() {
+    assert.isNaN(pow(2, 1.5));
+  });
+
+  // Mocha will run only this block with it.only
+  //it.only("5 in the power of 2 equals 25", function() {
+  //  assert.equal(pow(5, 2), 25);
+  //});
+  
 });
